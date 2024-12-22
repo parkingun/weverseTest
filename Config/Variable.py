b933dde198e97990a7bd04b125b58732afb7f1d6
parkingun <engi9364@naver.com>
2024-12-22 11:36:03 +0900

    feat(Variable):  공통적으로 사용할 상수 정의

---
+++ Config/Variable.py
@@ -0,0 +1,10 @@
+# 기본 도메인
+BASE_URL = "https://weverse.io/"
+
+# 계정 정보 딕셔너리
+ACCOUNTS = {
+    "test1": {"email": "engi9364@naver.com", "password": "test1357!@"},
+    "test2": {"email": "test2@benx.com", "password": "test1357!@"},
+    "test3": {"email": "test3@benx.com", "password": "test1357!@"},
+    "test4": {"email": "test4@benx.com", "password": "test1357!@"},
+}
