700bbd3e2f22e3ea4cadfbda955bd2097164de30
parkingun <engi9364@naver.com>
2024-12-22 11:40:13 +0900

    feat(TestCase): 로그인 테스트 케이스 추가

---
+++ TestCase/Test_Login.py
@@ -0,0 +1,51 @@
+from Utilities.DriverSetting import DriverSetting
+from Config import Variable
+from Pages.LoginPage import Login
+from Pages.BasePage import BasePage
+from Pages.MainPage import mainPage
+from API.GetAccountInfo import getAccount
+
+class Test_Login(DriverSetting, BasePage):
+
+    # 현재 테스트에 필요한 변수 정의
+    baseURL = Variable.BASE_URL
+    email = Variable.ACCOUNTS["test1"]["email"]
+    password = Variable.ACCOUNTS["test1"]["password"]
+
+    def test_login(self):
+
+        self.driver.get(self.baseURL)
+
+        # 테스트에 사용할 페이지 객체 생성
+        main_Page =  mainPage(self.driver)
+        login_Page = Login(self.driver)
+
+        # 메인 페이지에서 로그인 화면으로 이동 케이스
+        main_Page.test_Connect_Signin()
+
+        # 로그인 수행
+        login_Page.test_Login(self.email, self.password)
+
+        # 로그인 후 프로필 페이지로 이동
+        main_Page.test_Connect_Profile()
+
+        # 현재 브라우저의 쿠키를 가져옴
+        cookies = self.driver.get_cookies()
+
+        """ 쿠키값에서 원하는 데이터 추출하기 위한 함수 """
+
+        def get_cookie_value(cookies, target_name):
+            # filter를 사용해 원하는 쿠키 항목을 찾고, 첫 번째 항목의 value 값을 반환
+            filtered_items = list(filter(lambda item: item['name'] == target_name, cookies))
+            return filtered_items[0]['value'] if filtered_items else None
+
+        # 쿠키에서 필요한 토큰 및 디바이스 ID 추출
+        accessToken = get_cookie_value(cookies, "we2_access_token")
+        deviceId = get_cookie_value(cookies, "we2_device_id")
+
+        # API를 호출하여 계정 정보의 응답을 response에 저장
+        response = getAccount(accessToken, deviceId)
+
+        print("*******************************************************************")
+        print(f" ID = {self.email}, PW = {self.password}, WID = {response["wid"]} ")
+        print("*******************************************************************")
