0f7215b918b7038d720fb7fdde996ac8df194ba0
parkingun <engi9364@naver.com>
2024-12-29 10:35:37 +0900

    chore: 변수명 카멜케이스로 변경

--- Function/ExtractCookieData.py
+++ Function/ExtractCookieData.py
@@ -1,5 +1,5 @@
 """ 쿠키값에서 원하는 데이터 추출하기 위한 함수 """
-def get_cookie_value(cookies, target_name):
+def get_cookie_value(cookies, targetName):
     # filter를 사용해 원하는 쿠키 항목을 찾고, 첫 번째 항목의 value 값을 반환
-    filtered_items = list(filter(lambda item: item['name'] == target_name, cookies))
-    return filtered_items[0]['value'] if filtered_items else None
+    filteredItems = list(filter(lambda item: item['name'] == targetName, cookies))
+    return filteredItems[0]['value'] if filteredItems else None
